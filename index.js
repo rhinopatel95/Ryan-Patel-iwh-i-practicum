76100e94e9593f31ef98ece8593cc4e7f6f0f0d1
Ryan Patel <rhinopatel@gmail.com>
2026-04-08 15:29:46 +0100

    Add homepage route and Pug template
    Adds a new GET '/' route in index.js that calls the HubSpot CRM API (using PRIVATE_APP_ACCESS) to fetch custom object data and renders a new 'homepage' Pug template. Also adds views/homepage.pug which displays a table of object properties (name, type, colour) and includes a link to '/update-cobj'. API call errors are logged to the console.

--- index.js
+++ index.js
@@ -13,6 +13,20 @@ const PRIVATE_APP_ACCESS = '';
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 // * Code for Route 1 goes here
+app.get('/', async (_req, res) => {
+    const url = 'https://api.hubapi.com/crm/v3/objects/2-201040559?properties=name,type,colour';
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
+    try {
+        const resp = await axios.get(url, { headers });
+        const data = resp.data.results;
+        res.render('homepage', { title: 'Custom Objects | Integrating With HubSpot I Practicum', data });
+    } catch (err) {
+        console.error(err);
+    }
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
